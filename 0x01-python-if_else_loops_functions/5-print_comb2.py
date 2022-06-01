42f786cb78733366c6fc28bba16a8e98ffc8180c
leFrasch <mburu.johnn3@gmail.com>
2022-06-01 12:54:22 -0700

    00....99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,2 @@
 #!/usr/bin/python3
-for i in range(0, 100):
-    if i != 99:
-        print(f"{i:02d}, ", end="")
-    else:
-        print(f"{i:02d}")
+print(", ".join("{:02d}".format(i) for i in range(0, 100)))
