fe1209bfd613afd49ed500e90716c278bd199159
leFrasch <mburu.johnn3@gmail.com>
2022-06-01 12:56:22 -0700

    Two digit combinations

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for i in range(0, 10):
     for j in range((i+1), 10):
         if (i is not 8) or (j is not 9):
-            print(f"{i}{j}, ", end="")
+            print("{}{}, ".format(i, j), end="")
         else:
-            print(f"{i}{j}")
+            print("{}{}".format(i, j))
